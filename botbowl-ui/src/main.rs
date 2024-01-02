7a69eea362913e6800a380b39d5c7e6e1fdfae04
mattias bermell <mattias@modl.ai>
2024-01-02 23:12:05 +0100

    simple implementation of ratatui UI

--- botbowl-ui/src/main.rs
+++ botbowl-ui/src/main.rs
@@ -1,245 +1,269 @@
-use botbowl_engine::core::gamestate::{GameState, GameStateBuilder};
-use botbowl_engine::core::model::{
-    BallState, PlayerID, PlayerStatus, Position, TeamType, HEIGHT, WIDTH,
-};
-use botbowl_engine::core::table;
-
 use std::{
-    fmt::Write as fmt_Write,
-    io::{self, stdin, Stdout, Write},
+    io::{self, stdout, Stdout},
+    rc::Rc,
+    time::{Duration, Instant},
+    u16,
 };
-use termion::{self, color, input::TermRead, raw::RawTerminal};
-use termion::{event::Key, raw::IntoRawMode};
 
-const SQUARE_HEIGHT: usize = 2;
-const SQUARE_WIDTH: usize = 4;
+use botbowl_engine::core::{
+    game_runner::{BotGameRunner, BotGameRunnerBuilder, GameRunner},
+    model::{BallState, FieldedPlayer, Position, TeamType},
+};
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::*,
+    widgets::{
+        canvas::{Line, *},
+        *,
+    },
+};
 
-fn main() {
-    println!("Hello world!");
-    let mut state = GameStateBuilder::new()
-        .add_home_players(&[(1, 1), (2, 2), (3, 1)])
-        .add_away_players(&[(26, 15), (25, 14), (24, 15)])
-        .add_ball((3, 3))
-        .build();
-
-    state.step_positional(table::PosAT::StartMove, state.get_player_unsafe(1).position);
-    state.get_mut_player_unsafe(2).used = true;
-    state.get_mut_player_unsafe(4).status = PlayerStatus::Stunned;
-    state.get_mut_player_unsafe(5).status = PlayerStatus::Down;
-
-    let mut rend = Renderer::new();
-    rend.curser_pos = Some(Position::new((5, 5)));
-    rend.run_loop(&mut state);
+fn main() -> io::Result<()> {
+    App::run()
 }
 
-fn letter_to_ball_carrier(s: String) -> String {
-    match s.as_str() {
-        "H" => "\u{1E26}".to_owned(),
-        "A" => "Ä".to_owned(),
-        _ => unreachable!(),
-    }
+#[allow(dead_code)]
+struct App {
+    game: BotGameRunner,
 }
 
-fn pos_to_tty_coord(pos: Position) -> (u16, u16) {
-    let (mut x, mut y) = <(u16, u16)>::from(pos);
-    x *= SQUARE_WIDTH as u16;
-    y *= SQUARE_HEIGHT as u16;
-    x -= 2;
-    (x, y)
-}
-fn player_repr(game_state: &GameState, id: PlayerID) -> String {
-    let p = game_state.get_player_unsafe(id);
-
-    let mut player_char = {
-        match p.stats.team {
-            TeamType::Home => "H".to_owned(),
-            TeamType::Away => "A".to_owned(),
-        }
-    };
-    if p.status != PlayerStatus::Up {
-        player_char = player_char.to_lowercase();
-    } else if matches!(game_state.ball, BallState::Carried(id) if id == p.id) {
-        player_char = letter_to_ball_carrier(player_char);
+impl App {
+    fn new() -> App {
+        let runner = BotGameRunnerBuilder::new().build();
+        App { game: runner }
     }
 
-    let mut fmt = "".to_owned();
-    //if active player, set blinkning
-    match game_state.info.active_player {
-        Some(id) if id == p.id => {
-            write!(&mut fmt, "{}", termion::style::Blink).unwrap();
-        }
-        _ => (),
-    }
-
-    //Set color
-    match p.stats.team {
-        TeamType::Home => write!(&mut fmt, "{}", color::Fg(color::Red)).unwrap(),
-        TeamType::Away => write!(&mut fmt, "{}", color::Fg(color::LightBlue)).unwrap(),
-    }
-
-    let (prefix, suffix): (&str, &str) = match (p.used, p.status) {
-        (false, PlayerStatus::Up) => ("(", ")"),
-        (false, PlayerStatus::Down) => ("-", "-"),
-        (false, PlayerStatus::Stunned) => ("=", "="),
-        (true, PlayerStatus::Up) => (" ", " "),
-        (true, PlayerStatus::Down) => ("-", "-"),
-        (true, PlayerStatus::Stunned) => ("=", "="),
-    };
-    format!(
-        "{}{}{}{}{}{}",
-        fmt,
-        prefix,
-        player_char,
-        suffix,
-        termion::style::Reset,
-        color::Fg(color::Reset)
-    )
-}
-
-pub struct Renderer {
-    pub curser_pos: Option<Position>,
-}
-impl Renderer {
-    pub fn new() -> Renderer {
-        Renderer { curser_pos: None }
-    }
-
-    fn select_current_cursor(&self, state: &mut GameState) {
-        let pos = self.curser_pos.unwrap();
-    }
-    pub fn run_loop(&mut self, state: &mut GameState) {
-        let stdin = stdin();
-        let mut stdout = io::stdout().into_raw_mode().unwrap();
-        let mut pos = self.curser_pos.unwrap();
-
-        self.draw(&mut stdout, state);
-        stdout.flush().unwrap();
-
-        for c in stdin.keys() {
-            match c.unwrap() {
-                Key::Char('w') => pos += (0, -1),
-                Key::Char('a') => pos += (-1, 0),
-                Key::Char('d') => pos += (1, 0),
-                Key::Char('s') => pos += (0, 1),
-                Key::Char('q') => break,
-                _ => (),
-            }
-            if !pos.is_out() {
-                self.curser_pos = Some(pos);
-            }
-            self.draw(&mut stdout, state);
-            stdout.flush().unwrap();
-        }
-    }
-    pub fn draw(&mut self, stdout: &mut RawTerminal<Stdout>, state: &GameState) {
-        let (width, height) = termion::terminal_size().unwrap();
-        let min_width = 6 + SQUARE_WIDTH as u16 * 26;
-        assert!(
-            width > min_width,
-            "width ({}) should be > {}",
-            width,
-            min_width
-        );
-        let min_height = 2 + SQUARE_HEIGHT as u16 * 15;
-        assert!(
-            height > min_height,
-            "height ({}) should be > {}",
-            height,
-            min_height
-        );
-
-        // Get and lock the stdios.
-        // let stdin = io::stdin();
-        // let stdin = stdin.lock();
-        // let stderr = io::stderr();
-        // let mut stderr = stderr.lock();
-
-        write!(stdout, "{}", termion::clear::All).unwrap();
-
-        //draw crowd
-        let crowd_y = (HEIGHT * SQUARE_HEIGHT - 3) as u16;
-        let crowd_x = (WIDTH * SQUARE_WIDTH - 6) as u16;
-        let long_side = "▒".repeat(crowd_x as usize);
-        write!(stdout, "{}{}", termion::cursor::Goto(1, 1), long_side).unwrap();
-        write!(stdout, "{}{}", termion::cursor::Goto(1, crowd_y), long_side).unwrap();
-
-        for y in 1..crowd_y {
-            write!(stdout, "{}▒▒", termion::cursor::Goto(1, y)).unwrap();
-            write!(stdout, "{}▒▒", termion::cursor::Goto(crowd_x, y)).unwrap();
-        }
-        // draw vertical lines (TD zones and line of scrimmage)
-        Position::all_positions()
-            .filter(|pos| !pos.is_out())
-            .filter(|&Position { x, y: _ }| x == 1 || x == 13 || x == 25)
-            .map(pos_to_tty_coord)
-            .for_each(|(x, y)| {
-                write!(stdout, "{}|", termion::cursor::Goto(x + 4, y)).unwrap();
-            });
-
-        // draw horizontal lines (wings)
-        Position::all_positions()
-            .filter(|pos| !pos.is_out())
-            .filter(|&Position { x: _, y }| y == 4 || y == 11)
-            .map(pos_to_tty_coord)
-            .for_each(|(x, y)| {
-                write!(stdout, "{}-", termion::cursor::Goto(x + 2, y + 1)).unwrap();
-            });
-
-        //draw squares
-        Position::all_positions()
-            .filter(|pos| !pos.is_out())
-            .filter(|&Position { x, y }| y != 15 && x != 1)
-            .map(pos_to_tty_coord)
-            .for_each(|(x, y)| {
-                write!(stdout, "{}+", termion::cursor::Goto(x, y + 1)).unwrap();
-            });
-
-        // draw players
-        Position::all_positions()
-            .filter_map(|pos| state.get_player_id_at(pos))
-            .for_each(|id| {
-                let (x, y) = pos_to_tty_coord(state.get_player_unsafe(id).position);
-                let s = player_repr(state, id);
-                write!(stdout, "{}{}", termion::cursor::Goto(x + 1, y), s).unwrap();
-            });
-        // draw ball
-        match state.ball {
-            BallState::OnGround(p) => {
-                let (x, y) = pos_to_tty_coord(p);
-                write!(stdout, "{}-b-", termion::cursor::Goto(x + 1, y)).unwrap();
-            }
-            BallState::InAir(p) => {
-                let (x, y) = pos_to_tty_coord(p);
-                if state.get_player_at(p).is_some() {
-                    write!(stdout, "{}\u{1E07}", termion::cursor::Goto(x + 2, y)).unwrap();
-                } else {
-                    write!(stdout, "{}^b^", termion::cursor::Goto(x + 1, y)).unwrap();
+    pub fn run() -> io::Result<()> {
+        let mut terminal = init_terminal()?;
+        let mut app = App::new();
+        let mut last_tick = Instant::now();
+        let tick_rate = Duration::from_millis(100);
+        loop {
+            let _ = terminal.draw(|frame| app.ui(frame));
+            let timeout = tick_rate.saturating_sub(last_tick.elapsed());
+            if event::poll(timeout)? {
+                if let Event::Key(key) = event::read()? {
+                    if let KeyCode::Char('q') = key.code {
+                        break;
+                    }
                 }
             }
-            _ => (),
+            app.game.step();
+            if app.game.game_over() {
+                break;
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                app.on_tick();
+                last_tick = Instant::now();
+            }
+        }
+        restore_terminal()
+    }
+
+    fn on_tick(&mut self) {}
+
+    fn ui(&self, frame: &mut Frame) {
+        const ROWS: u16 = 15;
+        const COLS: u16 = 26;
+        let rect_size = frame.size();
+        let allowed_square_sizes = &[(10, 5), (8, 4), (6, 3), (4, 2), (2, 1)];
+        let (square_width, square_height) = allowed_square_sizes
+            .iter()
+            .find(|(w, h)| rect_size.width / COLS >= *w && rect_size.height / ROWS >= *h)
+            .unwrap_or(&(1, 2));
+        let pitch_width = square_width * COLS;
+        let pitch_height = square_height * ROWS;
+
+        let pitch_intermediate = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(0)
+            .constraints([
+                Constraint::Length((rect_size.height - pitch_height) / 2),
+                Constraint::Length(pitch_height),
+                Constraint::Length((rect_size.height - pitch_height) / 2),
+            ])
+            .split(frame.size());
+
+        let pitch = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(0)
+            .constraints([
+                Constraint::Length((rect_size.width - pitch_width) / 2),
+                Constraint::Length(pitch_width),
+                Constraint::Length((rect_size.width - pitch_width) / 2),
+            ])
+            .split(pitch_intermediate[1])[1];
+
+        let rows = split_rows(&pitch, *square_height, ROWS).to_vec();
+        let squares = rows
+            .iter()
+            .map(|row| split_cols(row, *square_width, COLS).to_vec())
+            .collect::<Vec<_>>();
+        for (y, rows) in squares.iter().enumerate() {
+            for (x, chunk) in rows.iter().enumerate() {
+                let pos = Position::from((x + 1, y + 1));
+                let bg_color = match (pos.x + pos.y) % 2 {
+                    0 => Color::Reset,
+                    _ => Color::Black,
+                };
+                let ball: bool = match self.game.get_state().ball {
+                    BallState::OffPitch => false,
+                    BallState::OnGround(p) => p == pos,
+                    BallState::Carried(_) => false,
+                    BallState::InAir(p) => p == pos,
+                };
+                let td = self.game.get_state().get_endzone_x(TeamType::Home) == pos.x
+                    || self.game.get_state().get_endzone_x(TeamType::Away) == pos.x;
+
+                if let Some(player) = self.game.get_state().get_player_at(pos) {
+                    frame.render_widget(self.player_canvas(player, bg_color), *chunk);
+                } else if ball {
+                    frame.render_widget(self.ball_canvas(bg_color), *chunk);
+                } else if td {
+                    let fg_color = match bg_color {
+                        Color::Reset => Color::DarkGray,
+                        _ => Color::DarkGray,
+                    };
+                    frame.render_widget(
+                        self.td_square_canvas(bg_color, fg_color, pos.y as usize),
+                        *chunk,
+                    );
+                } else {
+                    frame.render_widget(self.square_canvas(bg_color), *chunk);
+                }
+            }
+        }
+    }
+    fn td_square_canvas(&self, bg_color: Color, fg_color: Color, y: usize) -> impl Widget + '_ {
+        let td_chars = "    TOUCHDOWN     ".chars().collect::<Vec<_>>();
+
+        Canvas::default()
+            .background_color(bg_color)
+            .marker(Marker::Braille)
+            .paint(move |ctx| {
+                ctx.print(50.0, 50.0, td_chars[y].to_string().fg(fg_color));
+            }) // Needed to infer the return type
+            .x_bounds([0.0, 100.0])
+            .y_bounds([0.0, 100.0])
+    }
+    // touchdown 9 charactera
+    fn square_canvas(&self, bg_color: Color) -> impl Widget + '_ {
+        Canvas::default()
+            .background_color(bg_color)
+            .marker(Marker::Braille)
+            .paint(|_| {}) // Needed to infer the return type
+            .x_bounds([0.0, 100.0])
+            .y_bounds([0.0, 100.0])
+    }
+    fn ball_canvas(&self, bg_color: Color) -> impl Widget + '_ {
+        Canvas::default()
+            .background_color(bg_color)
+            .marker(Marker::Braille)
+            .paint(move |ctx| {
+                ctx.draw(&Circle {
+                    x: 50.0,
+                    y: 50.0,
+                    radius: 15.0,
+                    color: Color::Yellow,
+                });
+            })
+            .x_bounds([0.0, 100.0])
+            .y_bounds([0.0, 100.0])
+    }
+    fn player_canvas(&self, player: &FieldedPlayer, bg_color: Color) -> impl Widget + '_ {
+        let fg_color = match player.stats.team {
+            TeamType::Home => Color::Red,
+            TeamType::Away => Color::LightBlue,
         };
 
-        if let Some((x, y)) = self.curser_pos.map(pos_to_tty_coord) {
-            let white = color::Bg(color::White);
-            write!(stdout, "{}{} ", termion::cursor::Goto(x, y), white).unwrap();
-            write!(stdout, "{}{} ", termion::cursor::Goto(x + 2, y + 1), white).unwrap();
-            write!(stdout, "{}{} ", termion::cursor::Goto(x + 2, y - 1), white).unwrap();
-            write!(stdout, "{}{} ", termion::cursor::Goto(x + 4, y), white).unwrap();
-            write!(
-                stdout,
-                "{}{}",
-                termion::style::Reset,
-                color::Fg(color::Reset)
-            )
-            .unwrap();
-        }
-
-        write!(stdout, "{}", termion::cursor::Goto(1, 35)).unwrap();
+        Canvas::default()
+            .background_color(bg_color)
+            .marker(Marker::Braille)
+            .paint(move |ctx| draw_player(ctx, fg_color))
+            .x_bounds([0.0, 100.0])
+            .y_bounds([0.0, 100.0])
     }
 }
+fn split_rows(area: &Rect, row_height: u16, num_rows: u16) -> Rc<[Rect]> {
+    let list_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(0)
+        .constraints(
+            (0..num_rows)
+                .map(|_| Constraint::Length(row_height))
+                .collect::<Vec<_>>(),
+        );
 
-impl Default for Renderer {
-    fn default() -> Self {
-        Self::new()
-    }
+    list_layout.split(*area)
+}
+
+fn split_cols(area: &Rect, col_width: u16, num_cols: u16) -> Rc<[Rect]> {
+    let list_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .margin(0)
+        .constraints(
+            (0..num_cols)
+                .map(|_| Constraint::Length(col_width))
+                .collect::<Vec<_>>(),
+        );
+
+    list_layout.split(*area)
+}
+fn draw_player(ctx: &mut Context, fg_color: Color) {
+    //ctx.print(0.0, 0.0, format!("{},{}", x, y));
+    //Head
+    ctx.draw(&Circle {
+        x: 50.0,
+        y: 75.0,
+        radius: 15.0,
+        color: fg_color,
+    });
+    //Body
+    ctx.draw(&Line {
+        x1: 50.0,
+        y1: 60.0,
+        x2: 50.0,
+        y2: 30.0,
+        color: fg_color,
+    });
+    //Arms
+    ctx.draw(&Line {
+        x1: 10.0,
+        y1: 50.0,
+        x2: 90.0,
+        y2: 50.0,
+        color: fg_color,
+    });
+    //Righ leg
+    ctx.draw(&Line {
+        x1: 50.0,
+        y1: 30.0,
+        x2: 70.0,
+        y2: 0.0,
+        color: fg_color,
+    });
+    //Left leg
+    ctx.draw(&Line {
+        x1: 50.0,
+        y1: 30.0,
+        x2: 30.0,
+        y2: 0.0,
+        color: fg_color,
+    });
+}
+fn init_terminal() -> io::Result<Terminal<CrosstermBackend<Stdout>>> {
+    enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+    Terminal::new(CrosstermBackend::new(stdout()))
+}
+
+fn restore_terminal() -> io::Result<()> {
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+    Ok(())
 }
