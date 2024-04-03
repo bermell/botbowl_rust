92ca4974793c565b5cd4bcca8077d9d44d6db073
mattias bermell <mattias@modl.ai>
2024-04-03 22:37:32 +0200

    active and used players displayed differently

--- botbowl-ui/src/player_drawings.rs
+++ botbowl-ui/src/player_drawings.rs
@@ -8,10 +8,23 @@ use ratatui::{
 };
 
 fn player_style(player: &FieldedPlayer, game_state: &GameState) -> Style {
-    match player.stats.team {
-        TeamType::Home => Style::default().fg(Color::Red),
-        TeamType::Away => Style::default().fg(Color::LightBlue),
-    }
+    let is_active_player = is_active(player, game_state);
+    let is_used = player.used && !is_active_player;
+
+    let fg_team = match (player.stats.team, is_used) {
+        (TeamType::Home, false) => Color::Red,
+        (TeamType::Home, true) => Color::Indexed(52),
+        (TeamType::Away, false) => Color::LightBlue,
+        (TeamType::Away, true) => Color::Indexed(22),
+    };
+
+    Style::default().fg(fg_team)
+}
+
+fn is_active(player: &FieldedPlayer, game_state: &GameState) -> bool {
+    game_state
+        .get_active_player()
+        .map_or(false, |active| active.id == player.id)
 }
 fn is_carrier(player: &FieldedPlayer, game_state: &GameState) -> bool {
     match game_state.ball {
@@ -33,22 +46,30 @@ fn ball_1x1() -> Span<'static> {
 pub fn player_8x4<'a>(player: &FieldedPlayer, game_state: &GameState) -> Vec<Line<'a>> {
     let style = player_style(player, game_state);
     let is_carrier = is_carrier(player, game_state);
+    let border_style = Style::default().fg(Color::White);
+    let border = match is_active(player, game_state) {
+        true => Span::styled("|", border_style),
+        false => Span::styled(" ", border_style),
+    };
+
+    let fm =
+        |s: &'static str| Line::from(vec![border.clone(), Span::styled(s, style), border.clone()]);
 
     if player.status != PlayerStatus::Up {
-        let empty = Line::from(vec![Span::styled("        ", style)]);
-        let star = Line::from(vec![Span::styled(" ***    ", style)]);
-        let l3 = Line::from(vec![Span::styled(" ▄▄▁▃▃▬ ", style)]);
-        let l4 = Line::from(vec![Span::styled(" ▀▀▔▀▀▬ ", style)]);
+        let empty = fm("      ");
+        let star_ = fm("***   ");
+        let l3___ = fm("▄▄▁▃▃▬");
+        let l4___ = fm("▀▀▔▀▀▬");
         if player.status == PlayerStatus::Down {
-            return vec![empty.clone(), empty, l3, l4];
+            return vec![empty.clone(), empty, l3___, l4___];
         }
-        return vec![empty, star, l3, l4];
+        return vec![empty, star_, l3___, l4___];
     }
 
-    let l1 = Line::from(vec![Span::styled("   ▆▆   ", style)]);
-    let l2 = Line::from(vec![Span::styled(" --▐▌-- ", style)]);
-    let l3 = Line::from(vec![Span::styled("   ▐▌   ", style)]);
-    let l4 = Line::from(vec![Span::styled("  /  \\  ", style)]);
+    let l1 = fm("  ▆▆  ");
+    let l2 = fm("--▐▌--");
+    let l3 = fm("  ▐▌  ");
+    let l4 = fm(" /  \\ ");
 
     let l2_ball = Line::from(vec![Span::styled(" --▐▌-", style), ball_2x1()]);
 
@@ -61,21 +82,29 @@ pub fn player_8x4<'a>(player: &FieldedPlayer, game_state: &GameState) -> Vec<Lin
 pub fn player_6x3<'a>(player: &FieldedPlayer, game_state: &GameState) -> Vec<Line<'a>> {
     let style = player_style(player, game_state);
     let is_carrier = is_carrier(player, game_state);
+    let border_style = Style::default().fg(Color::White);
+    let border = match is_active(player, game_state) {
+        true => Span::styled("|", border_style),
+        false => Span::styled(" ", border_style),
+    };
+
+    let fm =
+        |s: &'static str| Line::from(vec![border.clone(), Span::styled(s, style), border.clone()]);
 
     if player.status != PlayerStatus::Up {
-        let empty = Line::from(vec![Span::styled("      ", style)]);
-        let star = Line::from(vec![Span::styled(" ***  ", style)]);
-        let l2 = Line::from(vec![Span::styled(" ▄▁▃▬ ", style)]);
-        let l3 = Line::from(vec![Span::styled(" ▀▔▀▬ ", style)]);
+        let empty = fm("    ");
+        let star = fm("*** ");
+        let l2 = fm("▄▁▃▬");
+        let l3 = fm("▀▔▀▬");
         if player.status == PlayerStatus::Down {
             return vec![empty, l2, l3];
         }
         return vec![star, l2, l3];
     }
 
-    let l1 = Line::from(vec![Span::styled("  ▆▆  ", style)]);
-    let l2 = Line::from(vec![Span::styled(" -▐▌- ", style)]);
-    let l3 = Line::from(vec![Span::styled("  /\\  ", style)]);
+    let l1 = fm(" ▆▆ ");
+    let l2 = fm("-▐▌-");
+    let l3 = fm(" /\\ ");
 
     let l2_ball = Line::from(vec![Span::styled(" -▐▌", style), ball_2x1()]);
     if is_carrier {
